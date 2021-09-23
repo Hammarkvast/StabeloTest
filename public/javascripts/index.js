77859d1e4254e898bfa91c279faf5509458e2998
Hammarkvast <tomham-3@student.ltu.se>
2021-09-23 18:56:57 +0200

    Added layout using containers

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 
-start_state = true;
-
+var start_state = true;
+var hashmap = new Map();
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -18,14 +18,15 @@ function createLevels() {
         }
       }
     }
+    start_state = false;
   }
 
   async function moveUp(elevator, floor) {
-      building = document.getElementById("elevators")
+      building = document.getElementById("elevators");
       elevatorId = "elevator" + elevator.toString();
-      for(var row = building.rows.length - 1; row > floor; row--) {
+      for(var row = building.rows.length - 1; row >= floor; row--) {
           //console.log("HERE")
-          for(var col = 1; col < 5; col++){
+          for(var col = 0; col < 5; col++){
               if(building.rows[row].cells[col].id == elevatorId) {
                 console.log("HERE")
                 await sleep(2000);
@@ -35,4 +36,43 @@ function createLevels() {
           }
       }
 
+  }
+
+  async function quiet(ms) {
+    await sleep(ms);
+  }
+
+  async function moveDown(elevator, floor) {
+    await sleep(22000);;
+    building = document.getElementById("elevators");
+    elevatorId = "elevator" + elevator.toString();
+    for(var row = 0; row <= (20 - floor); row++){
+      console.log("AWAKEN")
+      for(var col = 0; col < 5; col++) {
+        if(building.rows[row].cells[col].id == elevatorId) {
+          await sleep(2000);
+          building.rows[row].cells[col].id = "inactive"
+          building.rows[row + 1].cells[col].id = elevatorId;
+        }
+      }
+    }
+  }
+  
+  function addButtons(){
+    div = document.getElementById("div-small");
+    for(var i = 0; i<21; i++) {
+      console.log("TEST")
+      if (i === 0){
+        const btn = document.createElement("button");
+        btn.className = "btn btn-success btn-square-md";
+        btn.innerText = "B";
+        div.appendChild(btn);
+      } 
+      else{   
+        const btn = document.createElement("button");
+        btn.className = "btn btn-dark btn-square-md";
+        btn.innerText = i.toString();
+        div.appendChild(btn);
+      }
+    }
   }
