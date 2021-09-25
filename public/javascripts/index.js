499da2983ff0c39de8c895393f2c288bad554a2d
Hammarkvast <tomham-3@student.ltu.se>
2021-09-25 14:34:56 +0200

    code works, needs fine tuning

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,17 @@
 
 var start_state = true;
-var hashmap = new Map();
+hashmap = new Map();
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function createLevels() {
+async function createLevels() {
     x = document.getElementById("elevators")
     for(y = 0; y < 20; y++){
       var row = x.insertRow(0);
       for(z = 0; z < 5; z++) {
         var cell = row.insertCell(z);
-        if(y === 0) {
+        if(y === hashmap.get(z+1)) {
             cell.id = "elevator" + (z + 1).toString();
         }else {
             cell.id = "inactive";
@@ -22,6 +22,8 @@ function createLevels() {
   }
 
   async function moveUp(elevator, floor) {
+      console.log("moving to floor: " + floor.toString());
+      floor = 21 - floor;
       building = document.getElementById("elevators");
       elevatorId = "elevator" + elevator.toString();
       for(var row = building.rows.length - 1; row >= floor; row--) {
@@ -38,15 +40,11 @@ function createLevels() {
 
   }
 
-  async function quiet(ms) {
-    await sleep(ms);
-  }
-
   async function moveDown(elevator, floor) {
-    await sleep(22000);;
     building = document.getElementById("elevators");
     elevatorId = "elevator" + elevator.toString();
-    for(var row = 0; row <= (20 - floor); row++){
+    console.log(building.rows.length);
+    for(var row = 0; row < building.rows.length-2; row++){
       console.log("AWAKEN")
       for(var col = 0; col < 5; col++) {
         if(building.rows[row].cells[col].id == elevatorId) {
@@ -60,19 +58,55 @@ function createLevels() {
   
   function addButtons(){
     div = document.getElementById("div-small");
-    for(var i = 0; i<21; i++) {
-      console.log("TEST")
+    for(let i = 0; i<21; i++) {
+      //console.log("TEST")
       if (i === 0){
         const btn = document.createElement("button");
         btn.className = "btn btn-success btn-square-md";
         btn.innerText = "B";
+        //btn.onclick = console.log("Button" + i.toString() + "pressed");
+
+        btn.addEventListener("click", function(){moveElevator(i)})
+        //btn.onclick = moveElevator(i);
         div.appendChild(btn);
       } 
       else{   
         const btn = document.createElement("button");
         btn.className = "btn btn-dark btn-square-md";
         btn.innerText = i.toString();
+        btn.addEventListener("click", function(){moveElevator(i)})
         div.appendChild(btn);
       }
+    }
+  }
+
+  function initMap() {
+    //console.log("TEST")
+    min = Math.ceil(0);
+    max = Math.floor(20);
+    for(var i = 1; i < 6; i++) {
+      var location = Math.floor(Math.random() * (max - min) + min);
+      hashmap.set(i, location);
+      console.log(hashmap.get(i));
+    }
+  }
+
+  function moveElevator(floor) {
+    console.log("Move to floor: " + floor.toString());
+    closest_elevator = 0;
+    min_floor_dif = 100;
+    for(const [key, value] of hashmap) {
+      if (Math.abs(value-floor) < min_floor_dif) {
+        min_floor_dif = Math.abs(value-floor);
+        closest_elevator = key;
+      }
+    }
+    console.log(closest_elevator)
+    if(floor < hashmap.get(closest_elevator)) {
+      console.log("DOWN")
+      moveDown(closest_elevator, floor);
+    }else {
+      console.log("UP")
+      moveUp(closest_elevator, floor);
     }
   }
